e33e26283207409cd75ec473bc5d698bd232006f
andre <drezao@gmail.com>
2021-06-07 09:37:56 -0300

    make it better

---
+++ checkout/admin.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+from django.contrib import admin
+
+from .models import CartItem, Order, OrderItem
+
+# Register your models here.
+
+admin.site.register([CartItem, Order, OrderItem])
---
+++ lojavirtualdjango/urls.py
@@ -0,0 +1,36 @@
+"""lojavirtualdjango URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf import settings
+from django.views.static import serve as serve_static
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from core import views
+from catalog import views as views_catalog
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^contato/$', views.contact, name='contact'),
+    url(r'^entrar/$', LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^sair/$', LogoutView.as_view(template_name='index.html'), name='logout'),
+    url(r'^catalogo/', include('catalog.urls', namespace='catalog')),
+    url(r'^conta/', include('accounts.urls', namespace='accounts')),
+    url(r'^compras/', include('checkout.urls', namespace='checkout')),
+    url(r'^paypal/', include('paypal.standard.ipn.urls')),
+    url(r'^admin/', admin.site.urls),
+]
